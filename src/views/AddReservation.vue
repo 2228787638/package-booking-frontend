5cfbd894bc9b2b18a7566aba61de8c05bacb377f
2228787638 <2228787638@qq.com>
2019-07-25 23:46:24 +0800

    [feat: implement AddReservation]

---
+++ src/views/AddReservation.vue
@@ -0,0 +1,43 @@
+<template>
+  <div style="background:#eee;padding: 50px" >
+    <Card :bordered="false">
+
+      <Col span="5">
+        <el-input v-model="id" placeholder="请输入运单号"></el-input>
+      </Col>
+      <Col span="6">
+        <el-date-picker
+          v-model="time"
+          type="datetime"
+          placeholder="选择日期时间">
+        </el-date-picker>
+      </Col>
+      <Button @click="submit">预约</Button>
+    </Card>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AddReservation',
+  data () {
+    return {
+      id: '',
+      time:''
+    }
+  },
+  methods: {
+    submit () {
+      this.$store.dispatch('updateTimer', {
+        id: this.id,
+        status: 1,
+        time:this.time.getTime()
+      })
+      this.$router.push('/bookingList')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
